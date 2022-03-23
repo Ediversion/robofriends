c44662ca3d0a93d2835518ec88eda8136304ef1d
unknown <born4ice@hotmil.com>
2022-03-23 17:58:52 +0300

    Initial commit

---
+++ robofriends/src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Ed Smith',
+        username: 'smith',
+        email:'sa@email.com',
+    },
+    {
+        id: 2,
+        name: 'Salam Alekum',
+        username: 'Alekum',
+        email:'sa@email.com',
+    },
+    {
+        id: 3,
+        name: 'Jane Doe',
+        username: 'Doe',
+        email:'sa@email.com',
+    },
+    {
+        id: 4,
+        name: 'Steve Harvey',
+        username: 'Harvey',
+        email:'sa@email.com',
+    },
+    {
+        id: 5,
+        name: 'Smith Unit',
+        username: 'Unit',
+        email:'sa@email.com',
+    },
+    {
+        id: 6,
+        name: 'Curtis J',
+        username: '50 cent',
+        email:'sa@email.com',
+    },
+    {
+        id: 7,
+        name: 'Marshall Em',
+        username: 'Eminem',
+        email:'sa@email.com',
+    },
+    {
+        id: 8,
+        name: 'Jay Z',
+        username: 'Sean',
+        email:'sa@email.com',
+    },
+    {
+        id: 9,
+        name: 'Mister Carter',
+        username: 'Lil Wayne',
+        email:'sa@email.com',
+    },
+    {
+        id: 10,
+        name: 'Tip Harris',
+        username: 'TIP',
+        email:'sa@email.com',
+    },
+]
